eae09232f16f43aaeea5a4a07991377750002880
Etienne Combelles <etienne.combelles@cs-soprasteria.com>
2026-03-27 16:56:32 +0100

    RSPY-970: Removed Dockerfile for mockup and associated code

--- docker/eopf/build-dask-eopf.py
+++ docker/eopf/build-dask-eopf.py
@@ -54,10 +54,6 @@ class Image:
 
 # All possible processor images
 all_procs = {
-    "mockup": Image(
-        "ghcr.io/rs-python/dask/mockup/k8s",
-        "ghcr.io/rs-python/dask/mockup/local"
-    ),
     "l0": Image(
         "ghcr.io/rs-python/dask/l0/k8s",
         "ghcr.io/rs-python/dask/l0/local",
@@ -149,16 +145,8 @@ else:
 
 for proc, local_cluster in procs_to_build:
 
-    # Handle special cases
-    if (proc == "mockup") and local_cluster:
-        if build_all:
-            continue
-        raise RuntimeError("No LocalCluster image for mockup")
-
     def get_dockerfile() -> Path:
         """Return Dockerfile to use"""
-        if proc == "mockup":
-            return THIS_DIR / "Dockerfile.dask-eopf-mockup"
         if local_cluster:
             return THIS_DIR / "Dockerfile.dask-eopf-localcluster"
         # default
