920b8264c4a2d36346ad97420cba5ef3b4966740
nleconte-csgroup <86782407+nleconte-csgroup@users.noreply.github.com>
2026-05-05 16:41:02 +0200

    Feat/prepare new release (#86)
    * docs: update changelog
    
    * feat: update jupyterhub profile image
    
    * ci: lower specs
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- .github/common/resources/configure-minikube-deployment.sh
+++ .github/common/resources/configure-minikube-deployment.sh
@@ -20,7 +20,16 @@ APPS=rs-workflow-env/apps
 # Lower the CPU requests
 sed -i 's!: 0.1!: 0.001!g' "${APPS}/jupyterhub/values.yaml"
 sed -i 's!: 500m!: 1m!g' "${APPS}/prefect3-server/values.yaml"
-sed -i 's!: "150m"!: "1m"!g'\
-  "${APPS}/prefect3-worker-eopf/values.yaml"\
-  "${APPS}/prefect3-worker-general/values.yaml"\
+sed -i 's!: "150m"!: "1m"!g' \
+  "${APPS}/prefect3-worker-eopf/values.yaml" \
+  "${APPS}/prefect3-worker-general/values.yaml" \
   "${APPS}/prefect3-worker-staging/values.yaml"
+
+# Lower jupyter specs
+yq -i '.scheduling.userScheduler.replicas = 1' "${APPS}/jupyterhub/values.yaml"
+yq -i '.singleuser.profileList = [ .singleuser.profileList[0] ]' "${APPS}/jupyterhub/values.yaml"
+
+# Lower prefect specs
+yq -i '.worker.replicaCount = 1' "${APPS}/prefect3-worker-general/values.yaml"
+yq -i '.worker.replicaCount = 1' "${APPS}/prefect3-worker-staging/values.yaml"
+yq -i '.worker.replicaCount = 1' "${APPS}/prefect3-worker-eopf/values.yaml"
