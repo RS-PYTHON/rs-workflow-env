7ebdd90e0b71bc9b86db0e56829685674bec4c35
Julien Gaucher <139342493+jgaucher-cs@users.noreply.github.com>
2026-05-29 14:28:12 +0200

    RSPY-1044: Make DPR service compatible with multiple dask versions (#95)
    * environment for dask cpm
    
    * environment update
    
    * RSPY-1025 - New image for S1-ARD 1.2.0
    
    * Add a dedicated Dask image for CPM v3 (new proc: "cpm3")
    
    - WARNING: the CPM version is temporarily hardcoded to 3.0.0rc3
    - Rename the `cpm` image to `cpm2` (RSPY-1043)
    
    * Add dask-eopf-versions.json
    
    * Build dask base images from conf in json file
    
    * Remove dep_name from dask-eopf-versions.json
    
    * Refactor build_dask_eopf.py
    
    * Template python_version and dask_tag in Dockerfile.dask-eopf-localcluster
    
    * Change args order
    
    * Use specific ci/cd for code quality
    
    * Replace json conf file by yml
    
    * Build cpm images for local mode
    
    * back to only cluster
    
    * back to only cluster
    
    * RSPY-1025 - New image for S1-ARD 1.2.0
    
    * Create jupyter kernels
    
    * Create jupyter kernels
    
    * Install ipywidgets in venvs
    
    * Fix Sonar
    
    * Restore git tag
    
    ---------
    
    Co-authored-by: Alin BUTU <alin.butu@cs-soprasteria.com>
    Co-authored-by: Vincent Privat <vincent.privat@airbus.com>
    Co-authored-by: fcraheix <fabien.craheix@rs-python.com>

--- docker/eopf/build_dask_eopf.py
+++ docker/eopf/build_dask_eopf.py
@@ -37,7 +37,11 @@ DOCKER_SCRIPTS = THIS_DIR.parent / "scripts"
 
 @dataclass
 class Image:
-    """Docker image information"""
+    """Docker image information. Each Docker image is specific to a single processor."""
+
+    # Versions used by the processor
+    python_version: str
+    dask_version: str
 
     # Docker image name for cluster usage (registry)
     k8s_name: str
@@ -48,33 +52,56 @@ class Image:
     # Name of the LocalCluster image (for debugging)
     local_cluster_name: str = ""
 
-    # Used to identify the image to build
+    # This is used mainly to identify the files that are specific to the processor:
+    # requirements-${IMAGE2BUILD}.txt, post-install-${IMAGE2BUILD}.sh, ...
+    # But it's not used in the name of the docker image itself.
     image2build: str = ""
 
 
 # All possible processor images
 all_procs = {
-    "cpm": Image(
-        k8s_name="ghcr.io/rs-python/dask/cpm/k8s",
-        image2build="dask-cpm",
+    # Image for CPM >=2.7.0,<3
+    "cpm2": Image(
+        python_version="3.11.7",
+        dask_version="2026.1.2",
+        k8s_name="ghcr.io/rs-python/dask/cpm2/k8s",
+        local_name="ghcr.io/rs-python/dask/cpm2/local",
+        local_cluster_name="ghcr.io/rs-python/dask/cpm2/localcluster",
+        image2build="dask-cpm2",
+    ),
+    # Image for CPM >=3
+    # WARNING: Up to first official relase, release-candidate revision is hard-coded in requirements-dask-cpm3.txt
+    "cpm3": Image(
+        python_version="3.13.12",
+        dask_version="2026.1.2",
+        k8s_name="ghcr.io/rs-python/dask/cpm3/k8s",
+        local_name="ghcr.io/rs-python/dask/cpm3/local",
+        local_cluster_name="ghcr.io/rs-python/dask/cpm3/localcluster",
+        image2build="dask-cpm3",
     ),
     "l0": Image(
-        "ghcr.io/rs-python/dask/l0/k8s",
-        "ghcr.io/rs-python/dask/l0/local",
-        "ghcr.io/rs-python/dask/l0/localcluster",
-        "dask-l0",
+        python_version="3.11.7",
+        dask_version="2024.5.2",
+        k8s_name="ghcr.io/rs-python/dask/l0/k8s",
+        local_name="ghcr.io/rs-python/dask/l0/local",
+        local_cluster_name="ghcr.io/rs-python/dask/l0/localcluster",
+        image2build="dask-l0",
     ),
     "s1ard": Image(
-        "ghcr.io/rs-python/dask/s1ard/k8s",
-        "ghcr.io/rs-python/dask/s1ard/local",
-        "ghcr.io/rs-python/dask/s1ard/localcluster",
-        "dask-s1ard",
+        python_version="3.13.12",
+        dask_version="2026.1.2",
+        k8s_name="ghcr.io/rs-python/dask/s1ard/k8s",
+        local_name="ghcr.io/rs-python/dask/s1ard/local",
+        local_cluster_name="ghcr.io/rs-python/dask/s1ard/localcluster",
+        image2build="dask-s1ard",
     ),
     "s3olci": Image(
-        "ghcr.io/rs-python/dask/s3olci/k8s",
-        "ghcr.io/rs-python/dask/s3olci/local",
-        "ghcr.io/rs-python/dask/s3olci/localcluster",
-        "dask-s3olci",
+        python_version="3.11.7",
+        dask_version="2024.5.2",
+        k8s_name="ghcr.io/rs-python/dask/s3olci/k8s",
+        local_name="ghcr.io/rs-python/dask/s3olci/local",
+        local_cluster_name="ghcr.io/rs-python/dask/s3olci/localcluster",
+        image2build="dask-s3olci",
     ),
 }
 
@@ -183,7 +210,7 @@ for proc, local_cluster in procs_to_build:
             )
 
     # Docker image information
-    image = all_procs[proc]
+    image: Image = all_procs[proc]
 
     # Docker image name
     if local_cluster:
@@ -211,16 +238,13 @@ for proc, local_cluster in procs_to_build:
         "--build-context",
         f"docker-scripts={str(DOCKER_SCRIPTS)}",
         str(THIS_DIR),
+        "--build-arg", f"PYTHON_VERSION={image.python_version}",
+        "--build-arg", f"DASK_TAG={image.dask_version}"
     ]
 
     # Extract base image used from the target name (k8s or local) and add it as an argument
     if not local_cluster:
         command += ["--build-arg", f"BASE_IMAGE_TARGET={registry.rsplit('/', 1)[1]}"]
-
-    if proc == 'cpm':
-        command += ["--build-arg", "PYTHON_VERSION_DPR=3.11.7", "--build-arg", "DASK_TAG=2026.1.2"]
-    else:
-        command += ["--build-arg", "PYTHON_VERSION_DPR=3.11.7", "--build-arg", "DASK_TAG=2024.5.2"]
 
     # Build image
     run_command(command + labels)
